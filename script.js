41f6ed359d45eb30e613aba8e0c1a9735e633b43
Bijay-Gurung <stha4580@gmail.com>
2023-06-26 21:07:15 +0545

    Rock paper Scissor Prototype

---
+++ script.js
@@ -0,0 +1,57 @@
+let winMsg = 'Victory';
+let loseMsg = 'Crushing Defeat';
+let tieMsg = 'Tie';
+
+let moveList = ['Rock','Paper','Scissors'];
+let statusDisplay = document.querySelector('#status-head');
+let moveDisplays = document.querySelectorAll('.move-display h2');
+let buttons = document.querySelectorAll('button');
+
+function calcResult(move1,move2){
+    if(move1 === move2){
+        return tieMsg;
+    }
+    else if(
+        (move1 === 'Rock' && move2 === 'Scissors') ||
+        (move1 === 'Paper' && move2 === 'Rock') ||
+        (move1 === 'Scissors' && move2 === 'Paper')
+    ){
+        return winMsg;
+    }else{
+        return loseMsg;
+    }
+}
+
+function randomMove(){
+    return Math.floor(Math.random()*moveList.length);
+}
+
+function startGame(){
+    statusDisplay.innerHTML = 'Choose!';
+
+    for(let i=0; i < buttons.length; i++){
+        buttons[i].innerHTML = moveList[i];
+        buttons[i].style.display = "inline-block";
+        buttons[i].addEventListener('click',playRound);
+    }
+}
+
+function endGame(){
+    for(let i=0; i<buttons.length; i++){
+        buttons[i].style.display = "none";
+        buttons[i].removeEventListener('click',playRound);
+    }
+}
+
+function playRound(event){
+    const playerMove = event.target.innerHTML;
+    const computerMove = moveList[randomMove()];
+
+    moveDisplays[0].innerHTML = `Player: ${playerMove}`;
+    moveDisplays[1].innerHTML = `Computer: ${computerMove}`;
+
+    const result = calcResult(playerMove,computerMove);
+    statusDisplay.innerHTML= result;
+}
+
+startGame();
